8ffa0a2649e47b7bb5d33f0f4e9826cadc2671e7
Egor <egor.tkachev@wiseweb.by>
2025-02-06 15:16:53 +0300

    feat: add the ability to check profile for anonymus user

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsUserItself(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.id == obj.id
+        return request.method in SAFE_METHODS or request.user.id == obj.id
 
 
 class IsObjectOwner(BasePermission):
